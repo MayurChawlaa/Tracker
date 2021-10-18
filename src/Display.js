a2677625fde804cd8785b5a8abbd28d8cf49f538
AzureAD\MayurChawla <mayur.chawla@active.ai>
2021-10-18 14:36:00 +0530

    sign toggle working

--- src/Display.js
+++ src/Display.js
@@ -1,14 +1,27 @@
 import "./display_style.css";
 
 function Display(){
+    const signButton = (e) =>{
+        console.log(e.target.innerHTML);
+        if(e.target.innerHTML === "+"){
+            e.target.innerHTML = "−";
+            e.target.style.backgroundColor = "lightcoral";
+        }
+        else {
+            e.target.innerHTML = "+";
+            e.target.style.backgroundColor = "chartreuse";
+        }
+    }
     return(
         <div className="display-container">
             <div className="display-date" contentEditable="true">31st Aug, 21</div>
             <div className="display-tag">
                 <div className="display-tag-name" contentEditable="true">Salary</div>
-                <div className="display-tag-sign"><button>+</button></div>
+                <div className="display-tag-sign">
+                    <button className="display-tag-sign-button" onClick={(e)=>signButton(e)}>+</button>
+                </div>
             </div>
-            <div className="display-amount" contentEditable="true">35,939</div>
+            <div className="display-amount" contentEditable="true" inputMode="numeric">35939</div>
         </div>
     );
 }
