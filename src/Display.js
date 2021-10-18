e8759038d22af4c9bfe092ba5102c8b5e32939ab
AzureAD\MayurChawla <mayur.chawla@active.ai>
2021-10-18 12:09:29 +0530

    sign button for addition or subtraction

--- src/Display.js
+++ src/Display.js
@@ -4,7 +4,10 @@ function Display(){
     return(
         <div className="display-container">
             <div className="display-date">31st Aug,21</div>
-            <div className="display-tag">Salary</div>
+            <div className="display-tag">
+                <div className="display-tag-name">Salary</div>
+                <div className="display-tag-sign"><button>+</button></div>
+            </div>
             <div className="display-amount">35,145</div>
         </div>
     );
