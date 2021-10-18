d52a5e3d382b119a87d9901da5f7a8abf252a196
AzureAD\MayurChawla <mayur.chawla@active.ai>
2021-10-18 13:02:01 +0530

    changing content editable tag location

--- src/Display.js
+++ src/Display.js
@@ -3,12 +3,12 @@ import "./display_style.css";
 function Display(){
     return(
         <div className="display-container">
-            <div className="display-date">31st Aug,21</div>
+            <div className="display-date" contentEditable="true">31st Aug, 21</div>
             <div className="display-tag">
-                <div className="display-tag-name">Salary</div>
+                <div className="display-tag-name" contentEditable="true">Salary</div>
                 <div className="display-tag-sign"><button>+</button></div>
             </div>
-            <div className="display-amount">35,939</div>
+            <div className="display-amount" contentEditable="true">35,939</div>
         </div>
     );
 }
--- src/Tracker.js
+++ src/Tracker.js
@@ -3,7 +3,7 @@ function Tracker(){
     return(
         <div className="table">
             <div className="row header">
-                <div className="column">Mayur</div>
+                <div className="column first-column">Mayur</div>
                 <div className="column">Axis Bank</div>
                 <div className="column">Union Bank</div>
                 <div className="column">Cash</div>
@@ -11,96 +11,95 @@ function Tracker(){
                 <div className="column"><button>Add Source</button></div>
             </div>
             <div className="row">
-                <div className="column">Filters</div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
-            </div>
-            <div className="row">
+                <div className="column first-column">Filters</div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"><Display/></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
-                <div className="column">Total</div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true">Grand Total</div>
-            </div>
-            <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
+                <div className="column"><Display/></div>
                 <div className="column"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
             </div>
             <div className="row">
-                <div className="column">Goals =&gt;</div>
-                
+                <div className="column first-column">Total</div>
+                <div className="column"></div>
+                <div className="column"></div>
+                <div className="column"></div>
+                <div className="column"></div>
+                <div className="column">Grand Total</div>
+            </div>
+            <div className="row">
+                <div className="column first-column"></div>
+                <div className="column"></div>
+                <div className="column"></div>
+                <div className="column"></div>
+                <div className="column"></div>
+                <div className="column"></div>
+            </div>
+            <div className="row">
+                <div className="column first-column" contentEditable="true">Goals =&gt;</div>
                 <div className="column" contentEditable="true"></div>
                 <div className="column" contentEditable="true"></div>
                 <div className="column" contentEditable="true"></div>
