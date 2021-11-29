b6f4b4f8a7aa86fd2fd1f94ba64f4006cd60f5cf
AzureAD\MayurChawla <mayur.chawla@active.ai>
2021-11-29 12:04:39 +0530

    got the children

--- src/Tracker.js
+++ src/Tracker.js
@@ -1,135 +1,141 @@
 import Display from "./Display";
-function Tracker(){
+function Tracker() {
     const Sum = () => {
-            console.log("Sum");
-            const table = document.querySelector(".table");
-            //console.log(table.childElementCount,table.childNodes);
-            for(let i=1;i<table.childElementCount-3;i++)
-            {
-                console.log(table.childNodes[i]);
-                
-                let row = table.childNodes[i];
-                for(let j=1;j<row.childElementCount;j++)
-                {
-                    //columns
-                    let column = row.childNodes[j];
-                    console.log(column);
-                    //children of columns are date,tag and amount
-                    let container = column.childNodes[0];
-                    console.log(container);
-                    //console.log(container[0].childNodes);
-                }
+        console.log("Sum");
+        const table = document.querySelector(".table");
+        //console.log(table.childElementCount,table.childNodes);
+        for (let i = 1; i < table.childElementCount - 3; i++) {
+            //console.log(table.childNodes[i]);                
+            let row = table.childNodes[i];
+            for (let j = 1; j < row.childElementCount - 1; j++) {
+                //columns
+                let column = row.childNodes[j];
+                //console.log(column);
+                //children of columns are date,tag and amount
+                let container = column.childNodes[0];
+                console.log(container);
+
+                let ContainechildNodes = container.childNodes;
+                console.log(ContainechildNodes[2].innerText);
+
+                //console.log("has child : ", container.hasChildNodes);
+                //console.log(container.hasChildNodes());
+                // let m = container.childNodes[2].querySelector(".display-amount");
+                // console.log(m);
+                //let temp_container = container.cloneNode();
+                //console.log(temp_container.hasChildNodes());
             }
+        }
     };
-    return(
+    return (
         <div>
-        <div className="table">
-            <div className="row header">
-                <div className="column first-column">Mayur</div>
-                <div className="column">Axis Bank</div>
-                <div className="column">Union Bank</div>
-                <div className="column">Cash</div>
-                <div className="column">Stocks</div>
-                <div className="column"><button>Add Source</button></div>
+            <div className="table">
+                <div className="row header">
+                    <div className="column first-column">Mayur</div>
+                    <div className="column">Axis Bank</div>
+                    <div className="column">Union Bank</div>
+                    <div className="column">Cash</div>
+                    <div className="column">Stocks</div>
+                    <div className="column"><button>Add Source</button></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column">Filters</div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"><Display /></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column">Total</div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column">Grand Total</div>
+                </div>
+                <div className="row">
+                    <div className="column first-column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                </div>
+                <div className="row">
+                    <div className="column first-column" contentEditable="true">Goals =&gt;</div>
+                    <div className="column" contentEditable="true"></div>
+                    <div className="column" contentEditable="true"></div>
+                    <div className="column" contentEditable="true"></div>
+                    <div className="column" contentEditable="true"></div>
+                    <div className="column" contentEditable="true">Grand Total</div>
+                </div>
             </div>
-            <div className="row">
-                <div className="column first-column">Filters</div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"><Display/></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column">Total</div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column">Grand Total</div>
-            </div>
-            <div className="row">
-                <div className="column first-column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
-                <div className="column"></div>
-            </div>
-            <div className="row">
-                <div className="column first-column" contentEditable="true">Goals =&gt;</div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true"></div>
-                <div className="column" contentEditable="true">Grand Total</div>
-            </div>
-        </div>
-        <button onClick={Sum}>Sum</button>
+            <button onClick={Sum}>Sum</button>
         </div>
     )
 }
